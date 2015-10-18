ffb43f8ab2dcdb898e5b06b7a5938403e5f304be
tothradoslav <toth.radoslav@gmail.com>
2015-10-18 11:18:39 +0200

    Navigation Collection added

--- client/views/layouts/Navigation.js
+++ client/views/layouts/Navigation.js
@@ -9,11 +9,19 @@ Template.Navigation.helpers({
   currentDatabase() {
     return Template.instance().routeParameters.get().database;
   },
+  currentCollection() {
+    return Template.instance().routeParameters.get().collection;
+  },
   connections() {
     return Connections.find({}, {sort: {name: 1}});
   },
   databases() {
-    return Databases.find({}, {sort: {name: 1}});
+    var connection = Template.instance().routeParameters.get().connection;
+    return Databases.find({connection_id: connection._id}, {sort: {name: 1}});
+  },
+  collections() {
+    var database = Template.instance().routeParameters.get().database;
+    return Collections.find({database_id: database._id}, {sort: {name: 1}});
   }
 });
 
