475b9b8693f323d740fafd1376ce999b1c95ff47
David Durika <durika.d@gmail.com>
2015-10-22 09:52:42 +0200

    Icons: updated auto icon matcher

--- packages/drmongo-icons/Icons.js
+++ packages/drmongo-icons/Icons.js
@@ -1,20 +1,60 @@
 Icons = {
   forCollection(name) {
     let mapper = {
-      articles: 'fa fa-newspaper-o'
+      articles: 'fa fa-newspaper-o',
+
+      invoice: 'fa fa-usd',
+      invoices: 'fa fa-usd',
+      price: 'fa fa-usd',
+      prices: 'fa fa-usd',
+
+      house: 'fa fa-home',
+      rental: 'fa fa-home',
+
+      location: 'fa fa-map-marker',
+      locations: 'fa fa-map-marker'
     };
 
-    let match = _.find(mapper, (value, key) => {
+    let match = null;
+    const singular = _.singularize(name);
+    const nameLowerObj = s(name).toLowerCase();
+    const nameHumanizedArray = s(name).humanize().toLowerCase().value().split(' ');
+
+
+    // by mapper, exact
+    match = _.find(mapper, (value, key) => {
       return key == name.toLowerCase();
     });
+    if(match) return match;
 
-    const singular = _.singularize(name);
-    if(!match && faIconsList.indexOf(name) != -1) {
+
+    // by icon name, exact
+    if(faIconsList.indexOf(name) != -1) {
       match = faIconsList[faIconsList.indexOf(name)];
-    } else if (!match && faIconsList.indexOf(singular) != -1) {
+    } else if (faIconsList.indexOf(singular) != -1) {
       match = faIconsList[faIconsList.indexOf(singular)];
     }
+    if(match) return 'fa fa-' + match;
 
-    return match ? 'fa fa-' + match : 'fa fa-list';
+
+    // by mapper, partial
+    match = _.find(mapper, (value, key) => {
+      return nameLowerObj.include(key);
+    });
+    if(match) return match;
+
+
+    // by icon name, partial
+    match = _.find(nameHumanizedArray, (namePart) => {
+      return _.find(faIconsList, (iconName) => {
+        return namePart == iconName;
+      });
+    });
+    //log('> match', match);
+    if(match) return 'fa fa-' + faIconsList[faIconsList.indexOf(match)];
+
+
+
+    return 'fa fa-list';
   }
 };
