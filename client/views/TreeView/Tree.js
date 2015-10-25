abe4a27508ae714e4522bf531addf804844fe7d3
David Durika <durika.d@gmail.com>
2015-10-25 16:47:12 +0100

    treeView: auto open first document [closes #15]

--- client/views/TreeView/Tree.js
+++ client/views/TreeView/Tree.js
@@ -4,6 +4,8 @@ Template.Tree.onCreated(function () {
 var clipboard = null;
 
 Template.Tree.onRendered(function () {
+  // auto open first document
+  $(this.find('.toggle-children')).click();
 });
 
 Template.Tree.helpers({});
