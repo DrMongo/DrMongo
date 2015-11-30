9334103b13084f993b8cc46aa4269ca92ca3dfa3
tothradoslav <toth.radoslav@gmail.com>
2015-11-30 14:14:31 +0100

    BUG in filtering after reaching 100 history items

--- model/filterHistory.js
+++ model/filterHistory.js
@@ -1,7 +1,7 @@
 FilterHistory = new Mongo.Collection(dr.collectionNamePrefix + 'filterHistory');
 
 FilterHistory.after.insert(function(userId, doc) {
-  FilterHistory.find({name: null}, {sort: {createdAt: 1}, skip: 100}).forEach(function(item) {
+  FilterHistory.find({name: null}, {sort: {createdAt: -1}, skip: 100}).forEach(function(item) {
     FilterHistory.remove(item._id);
   });
 });
