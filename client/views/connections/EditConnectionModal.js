a96bd9c40269206475c75b145fe1f2832b3ec167
David Durika <durika.d@gmail.com>
2015-11-30 19:54:11 +0100

    connection: set mongo URI instead of host + port [closes #90]

--- client/views/connections/EditConnectionModal.js
+++ client/views/connections/EditConnectionModal.js
@@ -12,12 +12,18 @@ Template.EditConnectionModal.helpers({
 Template.EditConnectionModal.events({
   'submit form': function (e, t) {
     e.preventDefault();
+    const mongoUri = t.$('#mongo-uri').val();
+    let uri;
+    if(mongoUri) {
+      uri = MongodbUriParser.parse(mongoUri);
+    } else {
+      uri = {};
+    }
     Connections.update(this._id, {
       $set: {
         name: t.$('#name').val() || 'New Connection',
-        host: t.$('#host').val() || 'localhost',
-        port: t.$('#port').val() || '27017',
-        database: t.$('#database').val()
+        mongoUri: mongoUri || 'mongodb://localhost:27017',
+        database: uri.database || null
       }
     });
     Meteor.call('updateConnectionStructure', this._id, function (error, result) {
