f6308d297c586b117e6c96ba8ae651affe9f0d92
David Durika <durika.d@gmail.com>
2016-01-08 10:02:43 +0100

    Migrations: user custom collection name

--- server/migrations/migrationStartup.js
+++ server/migrations/migrationStartup.js
@@ -1,3 +1,7 @@
+Migrations.config({
+  collectionName: dr.collectionNamePrefix + 'migrations'
+});
+
 Meteor.startup(function() {
   Migrations.migrateTo('latest');
   //Migrations.migrateTo('1,rerun');
